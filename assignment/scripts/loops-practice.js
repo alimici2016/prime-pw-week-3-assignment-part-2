afd15ed8c253008545454f8bfc7893a1e8e350c7
alimici2016 <78386559+alimici2016@users.noreply.github.com>
2021-09-05 06:21:05 -0500

    Update loops-practice.js

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -1,5 +1,4 @@
-console.log('****** Loops Practice ******');
-
+console.log('****** Loops Practice *******');
 
 // 1. 'for' loop
 console.log('---- 1. For loops ----');
@@ -11,64 +10,72 @@ for (let i=0; i<4; i++) {
 }
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5
-console.log('count from 0 to 5');
 //   - Which part of the example loop do you need to change to do this?
+console.log('count from 0 to 5');
 for (let i=0; i<=5; i++){
-console.log(i);
+  console.log(i);
 }
-//You need to change the condition from 4 to greater than or equal to 5.
+//The index changes from 4 to 5.
+
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
 for (let i=3; i<=5; i++){
   console.log(i);
 }
-//You would need to change the initial expression to 3 and the condition must make it so 'i' only runs if it is less than or equal to 5.
+//Both the starting index and the max index change.
+
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 for (let i=2; i<=10; i+=2){
   console.log(i);
 }
-//The initialexpression begins with the number 2, the condition only runs if the variable 'i' is less than or equal to 10, and instead of an increment we plus equal 2.
+//The initial index starts at 2, the max index changes to 10, and instead of incrementing we go up by 2.
+
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-for (let i=5; i>= 0; i--){
+for (i=5; i=>0; i--){
   console.log(i);
+  if (i === 0) break;
 }
+
 // 2. For of loops
 console.log('---- 2. For Of loop ----');
-let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb'];
+let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
-for (let star of stars ) {
+console.log('Some stars:');
+for (let star of stars) {
   console.log(star);
 }
 
-//This is looping more than I want it to, maybe some answers in feedback.
 // 3. While loops
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
-
 console.log('Some stars using while:');
-let starIndex= 0;
-while (starIndex < stars.length){
-  console.log(stars[starIndex]);
+let starIndex = 0;
+while (starIndex < stars.length) {
+ console.log(stars[starIndex]);
   starIndex++;
 }
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-let index = 0;
-const max = 5;
-while (index <=5){
+let index = 0
+const max = 5
+while (index <= max) {
   console.log(index);
-index++;
+  index++;
 }
+/*
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
-index = 10;
-while (index => 5){
+console.log('count backwards from 10 to 5');
+index = 10
+const min = 5
+while (index => min) {
   console.log(index);
-index--;
+  index--;
+  if (index === 5) break;
 }
-//I'm not exactly sure why this isn't working, maybe I could get help in feedback!
